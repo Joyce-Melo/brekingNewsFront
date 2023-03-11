0c245231a6b039d5c876af4f027dbe010758d82e
Joyce Melo <joyce.santosm@hotmail.com>
2023-03-11 16:22:46 -0300

    Feat: Home componente created

---
+++ src/Datas.js
@@ -0,0 +1,30 @@
+export const news = [
+  {
+    title: "Primeira Noticia",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sodales dictum justo ac facilisis. Etiam sollicitudin pulvinar urna, lacinia ornare tellus faucibus sed. Nulla placerat velit eget nisl rutrum hendrerit. Mauris odio est, hendrerit quis accumsan ac, faucibus ac sem. Sed vehicula ullamcorper tortor. Vivamus quis metus vitae tellus cursus sollicitudin. Mauris mi tellus, bibendum quis dignissim quis, mattis non dui. Vivamus quam lacus, lobortis venenatis quam sit amet, rutrum fringilla mauris. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Suspendisse eu magna tempus orci rutrum ornare quis eu enim. Aliquam venenatis est et ex auctor aliquam. Etiam sit amet leo consequat, sodales diam in, malesuada sem. Cras risus purus, auctor ac dapibus et, finibus ut odio. Aenean facilisis odio vel enim porta venenatis. Maecenas quis sapien tincidunt, varius orci id, ullamcorper ligula. Sed facilisis urna vel fermentum cursus.",
+    image: "https://images.unsplash.com/photo-1566378246598-5b11a0d486cc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80",
+    likes: 2,
+    comments: 4, 
+  },
+  {
+    title: "Segunda Noticia",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sodales dictum justo ac facilisis. Etiam sollicitudin pulvinar urna, lacinia ornare tellus faucibus sed. Nulla placerat velit eget nisl rutrum hendrerit. Mauris odio est, hendrerit quis accumsan ac, faucibus ac sem. Sed vehicula ullamcorper tortor. Vivamus quis metus vitae tellus cursus sollicitudin. Mauris mi tellus, bibendum quis dignissim quis, mattis non dui. Vivamus quam lacus, lobortis venenatis quam sit amet, rutrum fringilla mauris. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Suspendisse eu magna tempus orci rutrum ornare quis eu enim. Aliquam venenatis est et ex auctor aliquam. Etiam sit amet leo consequat, sodales diam in, malesuada sem. Cras risus purus, auctor ac dapibus et, finibus ut odio. Aenean facilisis odio vel enim porta venenatis. Maecenas quis sapien tincidunt, varius orci id, ullamcorper ligula. Sed facilisis urna vel fermentum cursus.",
+    image: "https://images.unsplash.com/photo-1566378246598-5b11a0d486cc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80",
+    likes: 2,
+    comments: 4, 
+  },
+  {
+    title: "Terceira Noticia",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sodales dictum justo ac facilisis. Etiam sollicitudin pulvinar urna, lacinia ornare tellus faucibus sed. Nulla placerat velit eget nisl rutrum hendrerit. Mauris odio est, hendrerit quis accumsan ac, faucibus ac sem. Sed vehicula ullamcorper tortor. Vivamus quis metus vitae tellus cursus sollicitudin. Mauris mi tellus, bibendum quis dignissim quis, mattis non dui. Vivamus quam lacus, lobortis venenatis quam sit amet, rutrum fringilla mauris. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Suspendisse eu magna tempus orci rutrum ornare quis eu enim. Aliquam venenatis est et ex auctor aliquam. Etiam sit amet leo consequat, sodales diam in, malesuada sem. Cras risus purus, auctor ac dapibus et, finibus ut odio. Aenean facilisis odio vel enim porta venenatis. Maecenas quis sapien tincidunt, varius orci id, ullamcorper ligula. Sed facilisis urna vel fermentum cursus.",
+    image: "https://images.unsplash.com/photo-1566378246598-5b11a0d486cc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80",
+    likes: 2,
+    comments: 4, 
+  },
+  {
+    title: "Quarta Noticia",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sodales dictum justo ac facilisis. Etiam sollicitudin pulvinar urna, lacinia ornare tellus faucibus sed. Nulla placerat velit eget nisl rutrum hendrerit. Mauris odio est, hendrerit quis accumsan ac, faucibus ac sem. Sed vehicula ullamcorper tortor. Vivamus quis metus vitae tellus cursus sollicitudin. Mauris mi tellus, bibendum quis dignissim quis, mattis non dui. Vivamus quam lacus, lobortis venenatis quam sit amet, rutrum fringilla mauris. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Suspendisse eu magna tempus orci rutrum ornare quis eu enim. Aliquam venenatis est et ex auctor aliquam. Etiam sit amet leo consequat, sodales diam in, malesuada sem. Cras risus purus, auctor ac dapibus et, finibus ut odio. Aenean facilisis odio vel enim porta venenatis. Maecenas quis sapien tincidunt, varius orci id, ullamcorper ligula. Sed facilisis urna vel fermentum cursus.",
+    image: "https://images.unsplash.com/photo-1566378246598-5b11a0d486cc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80",
+    likes: 2,
+    comments: 4, 
+  },
+];
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react' //Executando o react
+import ReactDOM from 'react-dom/client' //manipulando a DOM
+import App from './App' //importando um componenete
+import './index.css' //importando o CSS
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render( //esse root está no index.html e erstarei renderizando o componenete App que está ali embaixo
   <React.StrictMode>
     <App />
   </React.StrictMode>,
