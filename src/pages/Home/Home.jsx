17bdf13a258a698b06f5af0d9bd679331acfac79
Joyce Melo <joyce.santosm@hotmail.com>
2023-03-12 15:43:03 -0300

    Feat: Card style added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import { Card } from "../../components/Card/Card";
 import { Navbar } from "../../components/Navbar/Navbar";
 import {news} from '../../Datas.js'
+import { HomeBody } from "./HomeStyled";
 
 
 
@@ -8,12 +9,15 @@ export default function Home() {
   return (
     <>
       <Navbar />
-        {news.map((item, index) =>{
+      <HomeBody>
+      {news.map((item, index) =>{
           return(
             <Card key={index} news={item} />
             // Enviando item por item para o meu componente cards, precisamos tbm passar uma chave, para que o react saiba qual card é qual, então no nosso caso, passaremos o index como chave
           )
         })}
+
+      </HomeBody>
       
     </>
   );
